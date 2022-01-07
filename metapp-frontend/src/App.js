e2481db677c222985427cd4a3aca9dc796505910
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-07 14:59:43 +0000

    eventbrite api call

--- metapp-frontend/src/App.js
+++ metapp-frontend/src/App.js
@@ -1,12 +1,28 @@
 import './App.css'; 
+import { useEffect } from 'react';
 
 import HeadTtitle from './components/HeadTitle/HeadTitle'
 
+const eventURL = `https://www.eventbriteapi.com/v3/users/me/?token=${process.env.REACT_APP_SECRET_NAME}`
+
 function App() {
+
+  useEffect(() => {
+
+    fetch(eventURL).then(res => console.log(res))
+    
+  }, [])
+
+
   return (
     <div className="App">
       <HeadTtitle/>
+
+      <div className="apiCall">
+      </div>
+
     </div>
+
   );
 }
 
