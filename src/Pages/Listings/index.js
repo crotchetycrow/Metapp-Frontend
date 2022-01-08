93dd14db01355eb19a017f52759d7f44a2571801
Phil Smithies <philsmithies@gmail.com>
2022-01-08 10:43:47 +0100

    listings page

---
+++ src/Pages/Listings/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Listings = () => {
+  return (
+    <div className="listings_wrapper">
+      <h1>Listings Here</h1>
+    </div>
+  );
+};
+
+export default Listings;
