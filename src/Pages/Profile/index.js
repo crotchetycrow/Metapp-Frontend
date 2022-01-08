0dac382976178bccdc09cc15a17f5fcbf3f4e6e8
Phil Smithies <philsmithies@gmail.com>
2022-01-08 10:52:46 +0100

    profile page updated

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,16 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 
-const eventURL = `https://jsonplaceholder.typicode.com/users`;
-
 function Profile() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    fetch(eventURL).then((res) => console.log(res));
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch(console.log);
   }, []);
+
+  console.log(users);
 
   return (
     <div className="profile_wrapper">
       <h1>Profile Page Here</h1>
+      {users.map((user) => (
+        <div>
+          <h3>User:</h3>
+          <p>Name:{user.name}</p>
+          <p>Username:{user.username}</p>
+        </div>
+      ))}
     </div>
   );
 }
