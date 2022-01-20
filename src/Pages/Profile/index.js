18e1cde34ad55aaf73ab8d7a12db7917b5f9caac
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-20 10:23:48 +0000

    Added search bar functionality

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 
 function Profile() {
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("")
 
   useEffect(() => {
     fetch("http://jsonplaceholder.typicode.com/users")
@@ -18,8 +19,15 @@ function Profile() {
   return (
     <div className="profile_wrapper">
       <h1>Profile Page Here</h1>
-      {users.map((user) => (
-        <div>
+      <input placeholder="Search users" onChange={event => setQuery(event.target.value)} />
+      {users.filter((user) => {
+        if (query === "") {
+          return user;
+        } else if (user.name.toLowerCase().includes(query.toLowerCase()) || user.username.toLowerCase().includes(query.toLowerCase())) {
+          return user
+        }
+      }).map((user, index) => (
+        <div className="box" key={index}>
           <h3>User:</h3>
           <p>Name:{user.name}</p>
           <p>Username:{user.username}</p>
